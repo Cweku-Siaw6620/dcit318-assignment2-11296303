1e5ca0499b8ee660346ab5a6a1b1da30ae9facd1
Kelvin Ashong <kksashong@st.ug.edu.gh>
2025-07-31 13:19:53 +0000

    Cretead the inheritance and method overide demostration

---
+++ InheritanceDemonstration/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class - Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class - Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    // Main entry point
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Should print "Some generic sound"
+            dog.MakeSound();           // Should print "Bark"
+            cat.MakeSound();           // Should print "Meow"
+        }
+    }
+}
